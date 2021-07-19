62e6f250921586a552b892ff9b48224e404aac9e
Sahil Bhatiya <64317865+SahilBhatiya@users.noreply.github.com>
2021-07-19 09:47:36 +0530

    Minor Fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,22 +3,22 @@ var scroll;
 window.onload = function() {
     document.getElementById("preloader").style.opacity = "0";
 
+    scroll = new LocomotiveScroll({
+        el: document.getElementById('body'),
+        smooth: true,
+        smartphone: {
+            smooth: true,
+            lerp: 1
+        },
+        tablet: {
+            smooth: true,
+            lerp: 1
+        },
+        lerp: 0.08
+    });
+
     setTimeout(() => {
         $("#preloader").remove();
         document.getElementById("body").style.opacity = "1";
-        scroll = new LocomotiveScroll({
-            el: document.getElementById('body'),
-            smooth: true,
-            smartphone: {
-                smooth: true,
-                lerp: 1
-            },
-            tablet: {
-                smooth: true,
-                lerp: 1
-            },
-            lerp: 0.08
-        });
-
     }, 500);
 }
