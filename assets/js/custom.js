2ffa01362f3b7591af299a1d33e0db13a89df2e3
Sahil Bhatiya <64317865+SahilBhatiya@users.noreply.github.com>
2021-07-19 15:02:26 +0530

    Added: User Menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,3 +34,59 @@ function ToogleMenu() {
 function ScrollToTop() {
     scroll.scrollTo("top");
 }
+
+async function DisplayMsg(cssClass, HeadingText, ContentText, timer, pos) {
+
+    /* cssClass => 0 - sucess
+                => 1 - danger
+                => 2 - warning
+                => 3 - info
+    */
+
+    /* 
+        pos => t -top
+            => b - bottom
+
+    */
+
+    let classes = ["bg-green", "bg-red shadow-red", "bg-yellow", "bg-blue-05"];
+
+    if (pos == "t") {
+        $("#AlertMsg").css("top", "0px");
+        $("#AlertMsg").css("bottom", "");
+    } else {
+        $("#AlertMsg").css("bottom", "0px");
+        $("#AlertMsg").css("top", "");
+    }
+
+
+
+    classes.forEach(element => {
+        $("#AlertMsg").removeClass(element);
+    });
+
+    $("#AlertMsg").addClass("show " + classes[cssClass]);
+    $("#DisplayAlertHeading").text(HeadingText);
+    $("#DisplayAlertContent").text(ContentText);
+
+    /* To Remove DisplayMsg After Sometime */
+    if (timer != 0) {
+        removeDisplayMsg(timer);
+    }
+}
+
+var DisplayMsgTimer;
+/* To Remove DisplayMsg */
+async function removeDisplayMsg(x) {
+    clearTimeout(DisplayMsgTimer);
+    DisplayMsgTimer = setTimeout(() => {
+        if ($("#AlertMsg").css("top") == "0px") {
+            $("#AlertMsg").css("top", "-100px");
+            $("#AlertMsg").css("bottom", "");
+        } else {
+            $("#AlertMsg").css("bottom", "-100px");
+            $("#AlertMsg").css("top", "");
+        }
+        $("#AlertMsg").removeClass("show");
+    }, x);
+}
